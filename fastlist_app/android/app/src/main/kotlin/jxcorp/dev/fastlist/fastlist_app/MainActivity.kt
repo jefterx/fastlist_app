e9fffc1ed8f81a130223b7c91eebdfa8dcfb795a
jefterx <marcojefter@gmail.com>
2023-01-03 16:26:45 -0400

    Primeiro commit

---
+++ fastlist_app/android/app/src/main/kotlin/jxcorp/dev/fastlist/fastlist_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package jxcorp.dev.fastlist.fastlist_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fastlist_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'app.dart';
+
+void main() => runApp(const FastListApp());
+
+class FastListApp extends StatelessWidget {
+  const FastListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const App();
+  }
+}
